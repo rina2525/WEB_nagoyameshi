8742308c6317686be16baec4451073bcbd501838
rina2525 <rina.yasuno@icloud.com>
2025-03-27 01:40:04 +0900

    Delete src directory

--- src/main/java/com/example/taberogu/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.taberogu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.taberogu.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer>{
-	public Role findByName(String name); 
-}
